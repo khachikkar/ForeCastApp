91c25db85739f8f3415e04cae997ec1bc0faa1bf
khachikkar <khachikkarapetyan78@gmail.com>
2024-10-23 03:26:05 +0400

    added alt prop

--- src/Components/Intro/index.js
+++ src/Components/Intro/index.js
@@ -5,7 +5,7 @@ const Intro = () => {
   return (
     <div className='wft'>
      <h2>See Weather Forecast</h2>
-     <img src='https://sun9-47.userapi.com/impg/TRPVPp9mxJn39PDQchwJl5lT5gY6uULREQyGnQ/cCw6A_NTsfs.jpg?size=807x427&quality=95&sign=20fcdff5f15d323bbc832262d2757890&c_uniq_tag=p7y8fckyAMdjEABAqWMgZmKn_el_qG37GZ_UxA7c07M&type=album'/>
+     <img src='https://sun9-47.userapi.com/impg/TRPVPp9mxJn39PDQchwJl5lT5gY6uULREQyGnQ/cCw6A_NTsfs.jpg?size=807x427&quality=95&sign=20fcdff5f15d323bbc832262d2757890&c_uniq_tag=p7y8fckyAMdjEABAqWMgZmKn_el_qG37GZ_UxA7c07M&type=album' alt='cd'/>
      <Link to="/weather">
      <button>See Weather</button>
      </Link>
